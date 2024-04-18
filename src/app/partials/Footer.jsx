f33a0d6f80557cd117c6e285afe064e0b933e517
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-04-17 21:14:05 -0400

    Conditional rendering and css fix

--- src/app/partials/Footer.jsx
+++ src/app/partials/Footer.jsx
@@ -36,7 +36,14 @@ const Developer = () => {
 	const developers = [
 		['Ruth Cedeño', 'BackEnd Dev', 'https://github.com/Ruth200218', '', '', ''],
 		['Richar Arenas', 'FullStack Dev', 'https://github.com/ErAo', 'https://www.linkedin.com/in/richarenas/', '', ''],
-		['Santiago Salazar', 'FrontEnd Dev', 'https://github.com/SantiagoCode', 'https://www.linkedin.com/in/santiagocode/', '', ''],
+		[
+			'Santiago Salazar',
+			'FrontEnd Dev',
+			'https://github.com/SantiagoCode',
+			'https://www.linkedin.com/in/santiagocode/',
+			'santiagosalazar.dev@gmail.com',
+			'+584123275955',
+		],
 	];
 
 	return (
@@ -46,21 +53,32 @@ const Developer = () => {
 					<Link href={dev_git} target='_blank'>
 						<img src={`${dev_git}.png`} width={120} height={120} alt={`${dev_name}-pic`} />
 					</Link>
+
 					<h3 className='dev_name'>{dev_name}</h3>
 					<p className='dev_position'>{dev_position}</p>
+
 					<div className='dev_media'>
-						<Link href={dev_git} target='_blank'>
-							<Icon.GitHub size={24} />
-						</Link>
-						<Link href={dev_link} target='_blank'>
-							<Icon.Linkedin size={24} />
-						</Link>
-						<Link href={''} target='_blank'>
-							<Icon.Mail size={24} />
-						</Link>
-						<Link href={''} target='_blank'>
-							<Icon.Phone size={24} />
-						</Link>
+						{dev_git !== '' && (
+							<Link href={dev_git} target='_blank'>
+								<Icon.GitHub size={24} />
+							</Link>
+						)}
+						{dev_link !== '' && (
+							<Link href={dev_link} target='_blank'>
+								<Icon.Linkedin size={24} />
+							</Link>
+						)}
+
+						{dev_mail !== '' && (
+							<Link href={`mailto:${dev_mail}`} target='_blank'>
+								<Icon.Mail size={24} />
+							</Link>
+						)}
+						{dev_phone !== '' && (
+							<Link href={`https://wa.me/${dev_phone}`} target='_blank'>
+								<Icon.Phone size={24} />
+							</Link>
+						)}
 					</div>
 				</div>
 			))}
