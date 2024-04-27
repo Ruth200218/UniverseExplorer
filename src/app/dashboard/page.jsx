868e306da89ae83655c77402042053d1128b15fe
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-04-27 12:48:30 -0400

    Dashboard list models from server side component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,38 +1,16 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
-import Earth from '../../components/Earth';
+import Dashboard_ListModels from '../../components/Dashboard_ListModels';
 
 const page = () => {
 	return (
 		<>
 			<h3>Tus ultimos modelos</h3>
 			<div className='dashboard__content-grid'>
-				<Items />
+				<Dashboard_ListModels />
 			</div>
 		</>
-	);
-};
-
-const Items = () => {
-	const list = [
-		['New System', 'Description', '/dashboard/model/new'],
-		['Solar System', 'Description', '/dashboard/model/solar_system'],
-		['Kepler-22', 'Description', '/dashboard/model/kepler_22'],
-		['Kepler-62', 'Description', '/dashboard/model/kepler_62'],
-	];
-
-	return (
-		<div className='dashboard__content-container'>
-			<Earth />
-			{list.map(([item, description, link]) => (
-				<Link href={link} className='dashboard_content-item'>
-					<h3>{item}</h3>
-					<p>{description}</p>
-				</Link>
-			))}
-		</div>
 	);
 };
 
