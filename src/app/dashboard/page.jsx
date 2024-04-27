1c6dcebfdf2514c38d63d436051f37fb0d843ace
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-04-27 12:49:09 -0400

    Make it server side component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,12 +1,10 @@
-'use client';
-
 import React from 'react';
 import Dashboard_ListModels from '../../components/Dashboard_ListModels';
 
 const page = () => {
 	return (
 		<>
-			<h3>Tus ultimos modelos</h3>
+			<h3>Your last models</h3>
 			<div className='dashboard__content-grid'>
 				<Dashboard_ListModels />
 			</div>
